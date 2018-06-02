64be238de8085b513ac4c358ced9a263e1674047
MYLS <wiryls@gmx.com>
2018-06-02 21:29:04 +0800

    add main scene

--- src/cc-h5/src/loadingui.ts
+++ src/cc-h5/src/loadingui.ts
@@ -1,51 +1,37 @@
-//////////////////////////////////////////////////////////////////////////////////////
-//
-//  Copyright (c) 2014-present, Egret Technology.
-//  All rights reserved.
-//  Redistribution and use in source and binary forms, with or without
-//  modification, are permitted provided that the following conditions are met:
-//
-//     * Redistributions of source code must retain the above copyright
-//       notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above copyright
-//       notice, this list of conditions and the following disclaimer in the
-//       documentation and/or other materials provided with the distribution.
-//     * Neither the name of the Egret nor the
-//       names of its contributors may be used to endorse or promote products
-//       derived from this software without specific prior written permission.
-//
-//  THIS SOFTWARE IS PROVIDED BY EGRET AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
-//  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-//  IN NO EVENT SHALL EGRET AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;LOSS OF USE, DATA,
-//  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-//  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-//////////////////////////////////////////////////////////////////////////////////////
+class LoadingUI extends egret.Sprite implements RES.PromiseTaskReporter
+{
+    private text: egret.TextField;
+    private counter: number = 0;
 
-class LoadingUI extends egret.Sprite implements RES.PromiseTaskReporter {
-
-    public constructor() {
+    public constructor(public groups: ReadonlyArray<string>)
+    {
         super();
-        this.createView();
+        this.onCreateView();
     }
 
-    private textField: egret.TextField;
-
-    private createView(): void {
-        this.textField = new egret.TextField();
-        this.addChild(this.textField);
-        this.textField.y = 300;
-        this.textField.width = 480;
-        this.textField.height = 100;
-        this.textField.textAlign = "center";
+    private onCreateView(): void
+    {
+        this.text = new egret.TextField();
+        this.addChild(this.text);
+        this.text.y = 300;
+        this.text.width = 480;
+        this.text.height = 100;
+        this.text.textAlign = "center";
     }
 
-    public onProgress(current: number, total: number): void {
-        this.textField.text = `Loading...${current}/${total}`;
+    public count(): void
+    {
+        if (this.counter < this.groups.length)
+            this.counter += 1;
+    }
+
+    public onProgress(current: number, total: number): void
+    {
+        const group = this.groups.length;
+        const index = this.counter;
+        const scale = 100.0 / group;
+        const percent = Math.round(scale * index + scale * current / total);
+
+        this.text.text = `Loading...${percent}%`;
     }
 }
