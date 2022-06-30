73d24c2211533f1ac63af551bef9cbf452e274b4
wiryls <wiryls@gmx.com>
2022-07-01 00:45:44 +0800

    fix: should sort by root index

--- cc-core/src/model/lookup.rs
+++ cc-core/src/model/lookup.rs
@@ -73,7 +73,7 @@ impl DisjointSet {
             .iter()
             .map(|&i| (i, Self::root(&self.parents, i)))
             .collect::<Box<_>>();
-        pairs.sort_by_key(|pair| pair.0);
+        pairs.sort_by_key(|pair| pair.1);
         Groups(pairs)
     }
 
@@ -124,14 +124,14 @@ pub struct Groups(Box<[(/* self */ usize, /* root */ usize)]>);
 impl Groups {
     pub fn iter<'a>(&'a self) -> GroupsIterator<'a> {
         GroupsIterator {
-            group: &self,
+            group: &self.0,
             index: 0,
         }
     }
 }
 
 pub struct GroupsIterator<'a> {
-    group: &'a Groups,
+    group: &'a [(usize, usize)],
     index: usize,
 }
 
@@ -139,41 +139,28 @@ impl<'a> Iterator for GroupsIterator<'a> {
     type Item = GroupIterator<'a>;
     fn next(&mut self) -> Option<Self::Item> {
         let lower = self.index;
-        let limit = self.group.0.len();
+        let limit = self.group.len();
         if limit <= lower {
             return None;
         }
 
         let mut upper = self.index + 1;
-        let value = self.group.0[lower];
-        while upper < limit && self.group.0[upper].1 == value.1 {
+        let value = self.group[lower];
+        while upper < limit && self.group[upper].1 == value.1 {
             upper += 1;
         }
 
         self.index = upper;
-        Some(GroupIterator {
-            group: self.group,
-            lower,
-            upper,
-        })
+        Some(GroupIterator(self.group[lower..upper].into_iter()))
     }
 }
 
-pub struct GroupIterator<'a> {
-    group: &'a Groups,
-    lower: usize,
-    upper: usize,
-}
+pub struct GroupIterator<'a>(std::slice::Iter<'a, (usize, usize)>);
 
 impl<'a> Iterator for GroupIterator<'a> {
     type Item = usize;
     fn next(&mut self) -> Option<Self::Item> {
-        let index = self.lower;
-        if self.upper <= index {
-            return None;
-        }
-        self.lower += 1;
-        Some(self.group.0[index].0)
+        self.0.next().map(|x| x.0)
     }
 }
 
@@ -190,23 +177,37 @@ mod test {
 
     #[test]
     fn basic_disjoint_set() {
-        let mut set = DisjointSet::new(10);
-        set.join(1, 3usize);
-        set.join(7, 9usize);
-        set.join(5, 7usize);
-        set.join(9, 3usize);
+        let cases = [
+            (
+                10,
+                vec![(1, 3), (7, 9), (5, 7), (9usize, 3usize)],
+                vec![vec![1, 3, 5, 7, 9usize]],
+            ),
+            (
+                10,
+                vec![(6, 1), (3, 1), (9, 1), (0, 2)],
+                vec![vec![0, 2], vec![1, 3, 6, 9]],
+            ),
+        ];
 
-        let groups = set.groups();
-        let mut it = groups.iter();
+        for (i, case) in cases.into_iter().enumerate() {
+            let mut set = DisjointSet::new(case.0);
+            for link in case.1 {
+                set.join(link.0, link.1);
+            }
 
-        let value = it.next();
-        assert!(value.is_some());
+            let mut out = set
+                .groups()
+                .iter()
+                .map(|x| {
+                    let mut x = x.collect::<Vec<_>>();
+                    x.sort();
+                    x
+                })
+                .collect::<Vec<_>>();
+            out.sort_by_key(|x| x.iter().copied().min());
 
-        let mut list = Vec::from_iter(value.unwrap());
-        list.sort();
-        assert_eq!(&list, &[1, 3, 5, 7, 9]);
-
-        let value = it.next();
-        assert!(value.is_none());
+            assert_eq!(case.2, out, "case {}", i);
+        }
     }
 }
